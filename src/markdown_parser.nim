0faad914c75b95a7dfbfdba70b381479a70b4a0f
Vasja Volin <vasja.volin@gmail.com>
2020-12-04 00:23:33 -0800

    start messing around with nim basics

--- src/markdown_parser.nim
+++ src/markdown_parser.nim
@@ -1,5 +1,11 @@
 # This is just an example to get you started. A typical binary package
 # uses this file as the main entry point of the application.
 
+proc printSomething(s: string): string =
+  var foo = "bar"
+  foo.add(s)
+  foo.add("baz")
+  return foo
+
 when isMainModule:
-  echo("Hello, World!")
+  echo printSomething("whee")
