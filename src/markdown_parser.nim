629c0941d5a3ea0f19b422390402558f043b5595
Vasja Volin <vasja.volin@gmail.com>
2020-12-07 18:23:33 -0800

    reworking the parser all the way from the start

--- src/markdown_parser.nim
+++ src/markdown_parser.nim
@@ -3,105 +3,59 @@
 
 import nre, strformat
 
+# so, we have learned some stuff. Let's start by testing a conversion of the
+# header token into a proper piece of html
 type
-  Token = object
-    kind: string
-    value: string
-proc `==`(a, b: Token): bool =
-  return (a.kind == b.kind) and (a.value == b.value)
+  Header = object
+    text: string
+    level: int
 
-type
-  TokenKind = object
-    kind: string
+  TokenTypes {.pure.} = enum
+    Header
+
+  Token = ref object
+    case kind: TokenTypes
+    of TokenTypes.Header: headerVal: Header
+
+  MatchRule = object
+    token: TokenTypes
     regex: string
 
-proc tokenize(s: string): seq[Token] =
-  const tokenKinds: seq[TokenKind] = @[
-    TokenKind(kind: "header", regex: "[\\n]?#+"),
-    TokenKind(kind: "text", regex: "[A-Za-z0-9 ]+"),
-    TokenKind(kind: "asterisk", regex: "\\*"),
-    TokenKind(kind: "underscore", regex: "_"),
-    TokenKind(kind: "lineEnding", regex: "\\n"),
-  ]
-  var text = s
-  var res: seq[Token]
+  ParseError = object of ValueError
 
-  block matching:
-    while text != "":
-      for tokenKind in tokenKinds:
-        let r = re("\\A({tokenKind.regex})".fmt)
-        let m = text.match(r)
+const matchRules = [
+  MatchRule(token: TokenTypes.Header, regex: "\\A(#{0,6}) *(\\w+)")
+]
 
-        if m != none(RegexMatch):
-          let capture = m.get.captures[0]
-          let length = len(capture)
-          text = text[length..^1]
+proc findToken(text: string, matcher: MatchRule): Token =
+  const value = Header(text: "h1", level: 1);
+  return Token(kind: TokenTypes.Header, headerVal: value)
 
-          res.add(Token(kind: tokenKind.kind, value: capture))
+proc parse*(text: var string): seq[Token] =
+  while len(text) > 0:
+    var token: Token
 
-  return res
+    for matcher in matchRules:
+      token = findToken(text, matcher)
 
-# proc generate(tokens: seq[Token]): string =
-#   for token in tokens:
-#     result &= renderToken(token)
-#   return "<h1>foo</h1>"
+      if token != nil:
+        result &= token
+        break
+
+      if token == nil:
+        raise newException(ParseError, "we got a bad token for {text}")
+
 
 when isMainModule:
   import unittest
 
-  suite "generation":
-    test "generating header html":
-      let tokens = @[
-        Token(kind: "header", value: "#"),
-        Token(kind: "text", value: " foo")
-        ]
+  suite "basic integration":
+    test "converts h6s":
+      var text = "###### h6"
+      var result = parse(text)
+      check result == "<h6>h6</h6>"
 
-      let html = generate(tokens)
-
-      check html == "<h1>foo</h1>"
-
-    test "generating bold text":
-      let tokens = @[
-        Token(kind: "asterisk", value: "*"),
-        Token(kind: "text", value: "foo"),
-        Token(kind: "asterisk", value: "*"),
-      ]
-
-      let html = generate(tokens)
-
-      check html == "<p><em>foo</em></p>"
-
-  suite "tokenization":
-    test "tokenizing a header":
-      let header = tokenize("# h1")
-
-      check header[0] == Token(kind: "header", value: "#")
-      check header[1] == Token(kind: "text", value: " h1")
-
-    test "tokenizing a long header":
-      let header = tokenize("### some text")
-
-      check header[0] == Token(kind: "header", value: "###")
-      check header[1] == Token(kind: "text", value: " some text")
-
-    test "tokenizing an asterisk":
-      let asterisk = tokenize("some words * some other words")
-
-      check asterisk[0] == Token(kind: "text", value: "some words ")
-      check asterisk[1] == Token(kind: "asterisk", value: "*")
-      check asterisk[2] == Token(kind: "text", value: " some other words")
-
-    test "tokenizing an underscore":
-      let underscore = tokenize("some words _word_")
-
-      check underscore[0] == Token(kind: "text", value: "some words ")
-      check underscore[1] == Token(kind: "underscore", value: "_")
-      check underscore[2] == Token(kind: "text", value: "word")
-      check underscore[3] == Token(kind: "underscore", value: "_")
-
-    test "tokenizing line endings":
-      let ending = tokenize("some words\nsome more words")
-
-      check ending[0] == Token(kind: "text", value: "some words")
-      check ending[1] == Token(kind: "lineEnding", value: "\n")
-      check ending[2] == Token(kind: "text", value: "some more words")
+    test "converts h1s":
+      var text = "# h1"
+      var result = parse(text)
+      check result == "<h1>h1</h1>"
