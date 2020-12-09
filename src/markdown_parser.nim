ad05e968c87d2ab21b3c05626edda962b28f69cb
Vasja Volin <vasja.volin@gmail.com>
2020-12-08 20:06:16 -0800

    move the tests to a dedicated file

--- src/markdown_parser.nim
+++ src/markdown_parser.nim
@@ -100,27 +100,3 @@ proc parse*(text: var string): string =
   let tokens: seq[Token] = tokenize(text)
   for tok in tokens:
     result &= renderToken(tok)
-
-when isMainModule:
-  import unittest
-
-  suite "basic integration":
-    test "converts paragraphs":
-      var text = "some text\n\nsome other text\n\nsome more text"
-      var res = parse(text)
-      check res == "<p>some text</p><p>some other text</p><p>some more text</p>"
-
-    test "converts h6s":
-      var text = "###### h6"
-      var res = parse(text)
-      check res == "<h6>h6</h6>"
-
-    test "converts h1s":
-      var text = "# h1"
-      var res = parse(text)
-      check res == "<h1>h1</h1>"
-
-    test "converts callouts":
-      var text = "```something\nsomething else```"
-      var res = parse(text)
-      check res == "<div class='callout'><p>something\nsomething else</p></div>"
--- watch.sh
+++ watch.sh
@@ -1,1 +1,1 @@
-fswatch ./src --event Updated | xargs -n1 -I{} nim c -r --verbosity:0 src/markdown_parser.nim
+fswatch ./src --event Updated | xargs -n1 -I{} nim c -r --verbosity:0 src/markdown_parser_test.nim
